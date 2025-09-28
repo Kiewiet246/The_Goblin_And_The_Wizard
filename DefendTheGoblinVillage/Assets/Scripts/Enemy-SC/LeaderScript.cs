6b93976f083747924173848292e423d8587e9278
Kiewiet246 <kiewiet.lombaard01@gmail.com>
2025-09-29 00:39:56 +0200

    Enemies are now in Parties and can Jump

--- DefendTheGoblinVillage/Assets/Scripts/Enemy-SC/LeaderScript.cs
+++ DefendTheGoblinVillage/Assets/Scripts/Enemy-SC/LeaderScript.cs
@@ -5,10 +5,16 @@ public class LeaderScript : MonoBehaviour
 {
     [Header("Target Movement")]
     [SerializeField] private List<TileInfo> waypoints;
-    [SerializeField] private Transform leaderTarget;
+    [SerializeField] private Vector3 leaderTarget;
     [SerializeField] private float distanceToTarget; //How far the Target is
     [SerializeField] private float closeEnough; //How far the leader needs to be to switch target
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float movementSpeed;
     
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float sightRange;
+    [SerializeField] private int layer;
     
     [Header("Followers")]
     [SerializeField] private List<EnemyContoller> followers;
@@ -26,24 +32,56 @@ public class LeaderScript : MonoBehaviour
 
     void FixedUpdate()
     {
+        MoveEnemy();
         CheckDistToTarget();
+        CheckForStep();
     }
 
-  
+    private void MoveEnemy()
+    {
+        if (leaderTarget != Vector3.zero)
+        {
+            Vector3 direction = leaderTarget - transform.position;
+            Vector3 movement = direction.normalized * (movementSpeed * Time.deltaTime);
+            Vector3 endPoint = transform.position + movement;
+
+            rb.MovePosition(endPoint);
+        }
+    }
+
     private void CheckDistToTarget()
     {
-        if (waypoints.Count > 0 && leaderTarget != null)
+        if (waypoints.Count > 0 && leaderTarget != Vector3.zero)
         {
-            distanceToTarget = Vector3.Distance(transform.position, leaderTarget.position);
+            Vector3 stablilizedTarget = new Vector3(leaderTarget.x, transform.position.y, leaderTarget.z);
+            distanceToTarget = Vector3.Distance(transform.position, stablilizedTarget);
 
             if (distanceToTarget <= closeEnough)
             {
                 waypoints.RemoveAt(0);
                 if (waypoints.Count > 0)
                 {
-                    leaderTarget = waypoints[0].transform;
-                    AssignTargetForFollowers();           
+                    leaderTarget = new Vector3(waypoints[0].transform.position.x, transform.position.y, waypoints[0].transform.position.z);
+                    //AssignTargetForFollowers();
                 }
+            }
+        }
+    }
+
+    private void CheckForStep()
+    {
+        if (leaderTarget != Vector3.zero)
+        {
+            LayerMask layerMask = 1 << layer;
+            RaycastHit hit = new RaycastHit();
+            Vector3 stablilizedTarget = new Vector3(leaderTarget.x, transform.position.y, leaderTarget.z);
+            Vector3 direction = (stablilizedTarget - transform.position).normalized;
+            Physics.Raycast(transform.position, direction, out hit, sightRange, layerMask);
+            Debug.DrawRay(transform.position, direction*sightRange, Color.red);
+            if (hit.collider != null)
+            {
+               // Debug.Log("Wall");
+                rb.AddForce(rb.transform.up * jumpForce, ForceMode.Impulse);
             }
         }
     }
@@ -57,8 +95,8 @@ public class LeaderScript : MonoBehaviour
                 waypoints.Add(newWaypoints[i]);
             }
             
-            leaderTarget = waypoints[0].transform;
-            AssignTargetForFollowers();
+            leaderTarget =  leaderTarget = new Vector3(waypoints[0].transform.position.x, transform.position.y, waypoints[0].transform.position.z);
+         //   AssignTargetForFollowers();
         }
         
     }
@@ -69,7 +107,7 @@ public class LeaderScript : MonoBehaviour
         {
             for (int i = 0; i < followers.Count; i++)
             {
-                followers[i].SetTarget(leaderTarget);
+                //followers[i].SetTarget(leaderTarget);
             }
         }
         else
