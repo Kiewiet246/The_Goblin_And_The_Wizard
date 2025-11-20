9b24c30540e82c7b85c1684e78b64662ce96d971
Kiewiet246 <kiewiet.lombaard01@gmail.com>
2025-11-20 21:06:13 +0200

    PauseMenu

--- DefendTheGoblinVillage/Assets/Scripts/SceneManager4.cs
+++ DefendTheGoblinVillage/Assets/Scripts/SceneManager4.cs
@@ -3,8 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager4 : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf == true)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    
     public void ReloadScene()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
@@ -15,6 +33,24 @@ public class SceneManager4 : MonoBehaviour
 
     public void StartGame()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
